c8a3e430ec7bd450596eb33726fe13409ce06b72
Liiioooo <preissegger.leo@a1.net>
2019-08-31 10:53:46 +0200

    add auth-callback component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,14 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import { AuthCallbackComponent } from './components/auth-callback/auth-callback.component';
 
 export const HOME_ROUTES: Routes = [
-	{path: '', component: HomeComponent}
+	{
+		path: '',
+		component: HomeComponent
+	},
+	{
+		path: 'auth-callback',
+		component: AuthCallbackComponent
+		// for authentication to work, in production there should be a rewrite to a empty HTML file,
+		// because bootstrapping the entire app for auth is useless
+	},
 ];
 
 @NgModule({
 	declarations: [
-		HomeComponent
+		HomeComponent,
+		AuthCallbackComponent
 	],
 	imports: [
 		RouterModule,
