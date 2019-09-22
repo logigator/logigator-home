af449b7f261677e68e3ac601c285f44d4e2ed12b
Stefan Mikulik <stefanmikulik0604@gmail.com>
2019-09-22 22:31:09 +0200

    implement basic structure and layout

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth/auth.service';
+
+@Component({
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+	constructor(private auth: AuthService) { }
+
+	ngOnInit() {
+	}
+
+	public loginGoogle() {
+		this.auth.authenticateGoogle();
+	}
+
+	public loginTwitter() {
+		this.auth.authenticateTwitter();
+	}
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {AuthService} from './services/auth/auth.service';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-	declarations: [],
+	declarations: [FooterComponent, HeaderComponent],
+	exports: [
+		HeaderComponent,
+		FooterComponent
+	],
 	imports: [
-		CommonModule
+		CommonModule,
+		RouterModule
 	]
 })
 export class SharedModule {
