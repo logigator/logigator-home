c57d8087a7cc078cef3e04ec41b2e5f8fc891f2a
Andreas Schloessl <andreas.sch4@gmail.com>
2019-09-25 15:52:40 +0200

    fixed tslint errors

--- src/app/modules/home/components/auth-callback/auth-callback.component.spec.ts
+++ src/app/modules/home/components/auth-callback/auth-callback.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AuthCallbackComponent } from './auth-callback.component';
 
 describe('AuthCallbackComponent', () => {
-  let component: AuthCallbackComponent;
-  let fixture: ComponentFixture<AuthCallbackComponent>;
+	let component: AuthCallbackComponent;
+	let fixture: ComponentFixture<AuthCallbackComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AuthCallbackComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [ AuthCallbackComponent ]
+		})
+			.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AuthCallbackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AuthCallbackComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
