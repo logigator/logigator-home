ce595c9f719409e08a373d2dd4c271fead361108
Andreas <andreas.sch4@gmail.com>
2019-10-28 18:49:27 +0100

    added icons and improved audit score removed content which we didn't own

--- src/app/modules/home/components/examples/examples.component.ts
+++ src/app/modules/home/components/examples/examples.component.ts
@@ -20,10 +20,10 @@ export class ExamplesComponent implements OnInit, OnDestroy {
 	public autoPlay = true;
 	public imageUrls: IImage[] = [
 		{
-			url: 'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+			url: ''
 		},
 		{
-			url: 'https://images.pexels.com/photos/326055/pexels-photo-326055.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+			url: ''
 		}
 	];
 
