5cb8cb958b0a9d13a583903f3a0ba62ad68f4cf3
Leo Preissegger <preissegger.leo@gmail.com>
2019-11-12 17:14:23 +0100

    fix things for ie

--- src/app/shared/directives/lazy-load/lazy-load.directive.ts
+++ src/app/shared/directives/lazy-load/lazy-load.directive.ts
@@ -1,4 +1,5 @@
-import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+import {Directive, ElementRef, Inject, Input, OnInit} from '@angular/core';
+import {WINDOW} from '../../injectable-window';
 
 @Directive({
 	selector: 'img[appLazyLoad],iframe[appLazyLoad]'
@@ -10,20 +11,26 @@ export class LazyLoadDirective implements OnInit {
 
 	private _intersectionObserver: IntersectionObserver;
 
-	constructor(private el: ElementRef<HTMLIFrameElement | HTMLImageElement>) { }
+	constructor(
+		private el: ElementRef<HTMLIFrameElement | HTMLImageElement>,
+		@Inject(WINDOW) private window: Window
+	) { }
 
 	ngOnInit(): void {
-		this._intersectionObserver = new IntersectionObserver((entries, observer) => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					const lazy = entry.target;
-					lazy.setAttribute('src', this.dataSrc);
-					this._intersectionObserver.unobserve(lazy);
-				}
+		if ('IntersectionObserver' in window) {
+			this._intersectionObserver = new IntersectionObserver((entries, observer) => {
+				entries.forEach(entry => {
+					if (entry.isIntersecting) {
+						const lazy = entry.target;
+						lazy.setAttribute('src', this.dataSrc);
+						this._intersectionObserver.unobserve(lazy);
+					}
+				});
 			});
-		});
-
-		this._intersectionObserver.observe(this.el.nativeElement);
+			this._intersectionObserver.observe(this.el.nativeElement);
+		} else {
+			this.el.nativeElement.setAttribute('src', this.dataSrc);
+		}
 	}
 
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -29,6 +29,23 @@
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 /**
+ * IE9, IE10 and IE11 requires all of the following polyfills.
+ */
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'regenerator-runtime/runtime';
+
+/**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  * because those flags need to be set before `zone.js` being loaded, and webpack
