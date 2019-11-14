7d202053ad3b53fac9dab0faa97270ecdc4aa21b
Andreas Schloessl <andreas.sch4@gmail.com>
2019-11-14 14:48:09 +0100

    removed console log

--- src/app/shared/models/translation/server-translation-loader.ts
+++ src/app/shared/models/translation/server-translation-loader.ts
@@ -8,7 +8,6 @@ import * as path from 'path';
 export class ServerTranslationLoader implements TranslateLoader {
 
 	getTranslation(lang: string): Observable<any> {
-		console.log(path.join(__dirname, '..', 'logigator-home', 'assets', 'i18n', lang + '.json'));
 		const file = fs.readFileSync(path.join(__dirname, '..', 'logigator-home', 'assets', 'i18n', lang + '.json')).toString();
 		const jsonData = JSON.parse(file);
 		return of(jsonData);
