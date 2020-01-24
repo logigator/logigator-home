952e1257de61b6153b8243acec649f628ec912c8
Leo Preissegger <preissegger.leo@gmail.com>
2020-01-24 22:10:36 +0100

    performance optimization + prerendering optimization

--- src/app/shared/directives/lazy-load/lazy-load.directive.ts
+++ src/app/shared/directives/lazy-load/lazy-load.directive.ts
@@ -1,5 +1,6 @@
-import {Directive, ElementRef, Inject, Input, OnInit} from '@angular/core';
+import {Directive, ElementRef, Inject, Input, OnInit, PLATFORM_ID} from '@angular/core';
 import {WINDOW} from '../../injectable-window';
+import {isPlatformServer} from '@angular/common';
 
 @Directive({
 	selector: 'img[appLazyLoad],iframe[appLazyLoad]'
@@ -13,10 +14,12 @@ export class LazyLoadDirective implements OnInit {
 
 	constructor(
 		private el: ElementRef<HTMLIFrameElement | HTMLImageElement>,
-		@Inject(WINDOW) private window: Window
+		@Inject(WINDOW) private window: Window,
+		@Inject(PLATFORM_ID) private platformId: string
 	) { }
 
 	ngOnInit(): void {
+		if (isPlatformServer(this.platformId)) return;
 		if ('IntersectionObserver' in window) {
 			this._intersectionObserver = new IntersectionObserver((entries, observer) => {
 				entries.forEach(entry => {
