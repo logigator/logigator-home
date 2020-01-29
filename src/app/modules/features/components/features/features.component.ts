bf257d81dee5b2a69fe768eecfa95471d140b283
Andreas <andreas.sch4@gmail.com>
2020-01-29 23:21:56 +0100

    added title service to features page

--- src/app/modules/features/components/features/features.component.ts
+++ src/app/modules/features/components/features/features.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
 	selector: 'app-features',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
 
-	constructor() { }
+	constructor(private titleService: Title) { }
 
 	ngOnInit() {
+		this.titleService.setTitle('Logigator - Features');
 	}
 
 }
