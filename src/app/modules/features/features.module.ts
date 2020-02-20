156f5b515e82a39d809495d7f844e47d3d1ed0a1
Leo Preissegger <preissegger.leo@gmail.com>
2020-02-20 14:27:15 +0100

    update feature page for videos

--- src/app/modules/features/features.module.ts
+++ src/app/modules/features/features.module.ts
@@ -4,13 +4,15 @@ import { FeaturesComponent } from './components/features/features.component';
 import {FeaturesRoutingModule} from './features-routing.module';
 import {TranslateModule} from '@ngx-translate/core';
 import { FeatureComponent } from './components/feature/feature.component';
+import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
 	declarations: [FeaturesComponent, FeatureComponent],
 	imports: [
 		FeaturesRoutingModule,
 		CommonModule,
-		TranslateModule
+		TranslateModule,
+		SharedModule
 	]
 })
 export class FeaturesModule { }
