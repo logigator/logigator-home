355f184feab368504890fd3f7a062cc0ce19fc9d
Leo Preissegger <preissegger.leo@gmail.com>
2020-03-10 08:49:30 +0100

    add electron screenshots

--- src/app/modules/download/components/download/download.component.ts
+++ src/app/modules/download/components/download/download.component.ts
@@ -1,22 +1,26 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {EditorReleaseData} from '../../../../shared/models/http-responses/editor-release-data';
 import {ApiService} from '../../../../shared/services/api/api.service';
+import {ThemingService} from '../../../../shared/services/theming/theming.service';
 
 @Component({
 	selector: 'app-download',
 	templateUrl: './download.component.html',
-	styleUrls: ['./download.component.scss'],
-	changeDetection: ChangeDetectionStrategy.OnPush
+	styleUrls: ['./download.component.scss']
 })
 export class DownloadComponent implements OnInit {
 
 	public editorReleaseData$: Observable<EditorReleaseData>;
 
-	constructor(private api: ApiService) { }
+	constructor(private api: ApiService, private themingService: ThemingService) { }
 
 	ngOnInit() {
 		this.editorReleaseData$ = this.api.getEditorReleaseData();
+	}
+
+	public get currentTheme(): string {
+		return this.themingService.currentTheme;
 	}
 
 }
